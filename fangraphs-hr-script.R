ce1061293855784ab8fd00a062d9b8300c5df80a
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2022-03-26 23:52:51 -0400

    Add files via upload

---
+++ fangraphs-hr-script.R
@@ -0,0 +1,26 @@
+library(rstan)
+library(readr)
+library(car)
+library(tidyverse)
+
+# data scraped in python with pybaseball package
+
+df <- read_csv("C:/Users/david/Downloads/test_py.csv")
+
+stan_data = list(N = length(df$is_hr),
+                 home_run = df$is_hr,
+                 exit_velocity = df$launch_speed,
+                 launch_angle = df$launch_angle)
+
+system.time({
+fit <- stan(file = "fangraphs-hr-stan.stan",
+            data = stan_data,
+            warmup = 1000,
+            iter = 2000,
+            chains = 4,
+            cores = 6)
+})
+
+fit
+stan_hist(fit)
+
